59bf8d37cb54bf383771a8b5164ef4cc50be2c86
ホアンゴックシン <hoangngocsinh@sinhhn@brave.local>
2017-06-28 22:57:38 +0900

    add

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -10,8 +10,7 @@
             <div class="jumbotron">
                 <h1>Welcome to My Blog!</h1>
                 <p class="lead">Thank you so much for visiting. This is my test website built with Laravel. Please
-                    read
-                    my popular post!</p>
+                    read my popular post!</p>
                 <p><a class="btn btn-primary btn-lg" href="#" role="button">Popular Post</a></p>
             </div>
         </div>
